b2af7576f46f1f40e3a88b34c99cb54c36e6b849
Al Trujillo <Trujillo0525@gmail.com>
2019-10-25 16:36:24 +0000

    Imageblur#1

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+class Image
+arr = [1,2,3,4,5,6,7,8,9]
+
+def shuffle
+ @arr.shuffle
+end 
+
+puts arr=shuffle
---
+++ pix.rb
@@ -0,0 +1,5 @@
+row1 = [0,0,0,0]
+row2 = [0,1,0,0]
+
+ Image = [row1, row2]
+puts Image
